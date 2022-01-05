66097fe5d836179693c4c89dd98c872583b38309
KI YOUNG KIM <82855634+KIM7751@users.noreply.github.com>
2022-01-05 20:11:23 +0900

    01/05

---
+++ Web/colors.js
@@ -0,0 +1,39 @@
+var Links = {
+    setColor : function(color) {
+        var alist = document.querySelectorAll('a');
+    var i = 0;
+    while(i < alist.length) {
+        alist[i].style.color = color;
+        i += 1;
+       }
+    }
+}
+
+
+var Body = {
+    setColor : function(color) { 
+        document.querySelector('body').style.color = color;
+    },
+    setBackgroundColor : function(color) {
+        document.querySelector('body').style.backgroundColor = color;
+    }
+}
+
+
+
+function nightDayhandler(self) {
+    if (self.value === 'night'){
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    self.value ='day';
+
+    Links.setColor('powderblue');
+
+} else { 
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    self.value ='night';
+
+    Links.setColor('blue');
+}
+}
