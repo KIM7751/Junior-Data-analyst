a7a5694d4793428f90aca58bcebc8dec263d88ef
KI YOUNG KIM <82855634+KIM7751@users.noreply.github.com>
2022-01-06 18:09:36 +0900

    01/06

--- Web/colors.js
+++ Web/colors.js
@@ -1,21 +1,24 @@
 var Links = {
     setColor : function(color) {
-        var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length) {
-        alist[i].style.color = color;
-        i += 1;
-       }
+    //     var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length) {
+    //     alist[i].style.color = color;
+    //     i += 1;
+    //    }
+    $('a').css('color', color);
     }
 }
 
 
 var Body = {
     setColor : function(color) { 
-        document.querySelector('body').style.color = color;
+        // document.querySelector('body').style.color = color;
+        $('body').css('color', color);
     },
     setBackgroundColor : function(color) {
-        document.querySelector('body').style.backgroundColor = color;
+        // document.querySelector('body').style.backgroundColor = color;
+        $('body').css('backgroundColor', color);
     }
 }
 
